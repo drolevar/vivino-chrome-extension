4dff8ade28d5c3aa50f20400c1db1d8d1f6c34fc
Charaka Goonatilake <charakag@gmail.com>
2020-07-25 08:35:30 +0100

    Support for Laithwaites & Virgin Wines. Includes code cleanup.

--- background.js
+++ background.js
@@ -5,26 +5,43 @@ chrome.runtime.onMessage.addListener(
       var url = "https://www.vivino.com/search/wines?q=" + encodeURIComponent(request.wineName);
       fetch(url)
           .then(response => response.text())
-          .then(text => parseRating(text))
-          .then(response => sendResponse(response))
-          .catch(error => console.error(error))
+          .then(responseText => parseVivinoRating(responseText))
+          .then(ratingAndReviewCount => sendResponse(ratingAndReviewCount))
+          .catch(error => console.error("Error querying Vivino = " + error))
       return true;
     }
   }
 );
 
 // Parse the rating from the page returned by Vivino
-function parseRating(html) {
+function parseVivinoRating(html) {
   // Initialize the DOM parser
-  var parser = new DOMParser();
+  const parser = new DOMParser();
 
   // Parse the text
-  var document = parser.parseFromString(html, "text/html");
-  
-  // Find average rating
-  var rating = document.getElementsByClassName('text-inline-block light average__number')[0].innerHTML.trim();
+  const document = parser.parseFromString(html, "text/html");
 
-  // Find number of reviews
-  var ratingCount = document.getElementsByClassName('text-inline-block average__stars')[0].getElementsByClassName('text-micro')[0].innerHTML.trim();
-  return [rating, ratingCount];
+  const averageContainersElements = document.getElementsByClassName('average__container');
+  for (var i = 0, l = averageContainersElements.length; i < l; i++) {
+    // Find average rating
+    const ratingElements = averageContainersElements[i].getElementsByClassName('text-inline-block light average__number');
+
+    // Find number of reviews
+    const reviewCountElements = averageContainersElements[i].getElementsByClassName('text-inline-block average__stars');
+
+    if (ratingElements.length > 0 && reviewCountElements.length > 0) {
+      const ratingStr = ratingElements[0].innerHTML.trim();
+      const rating = parseFloat(ratingStr);
+      if (isNaN(rating)) continue;
+
+      const reviewCountStr = reviewCountElements[0].getElementsByClassName('text-micro')[0].innerHTML.trim(); 
+      if (reviewCountStr.includes('ratings')) {
+        const reviewCount = parseInt(reviewCountStr.slice(0, reviewCountStr.indexOf(' ')));
+        if (isNaN(reviewCount)) continue;
+         return [rating, reviewCount];
+      }
+    }  
+  }
+
+  return [0.0, 0];
 }
--- content.js
+++ content.js
@@ -1,24 +1,52 @@
-// Find and iterate through all the wines listed on the page
-var productHeaders = document.getElementsByClassName('product-details__header');
-for (var i = 0, l = productHeaders.length; i < l; i++) {
-  var wineNameElement = productHeaders[i].getElementsByClassName('space-b--none')[0];
+processUpdates(document.URL);
 
-  insertRating(wineNameElement);
+
+function processUpdates(url) {
+  if (url.includes('majestic')) { // YES
+    handleMajestic();
+  } else if (url.includes('laithwaites')) { // YES
+    handleLaithwaites();
+  } else if (url.includes('virginwines')) { // YES
+    handleVirginWines();
+  }
+}
+
+function handleMajestic() {
+  // Find and iterate through all the wines listed on the page
+  const wineElements = document.getElementsByClassName('product-details__header');
+  for (var i = 0, l = wineElements.length; i < l; i++) {
+    insertRating(wineElements[i].getElementsByClassName('space-b--none')[0]);
+  }
+}
+
+function handleLaithwaites() {
+  // Find and iterate through all the wines listed on the page
+  const wineElements = document.getElementsByClassName('print-item');
+  for (var i = 0, l = wineElements.length; i < l; i++) {
+      insertRating(wineElements[i].getElementsByTagName('h3')[0]);
+  }
+}
+
+function handleVirginWines() {
+  // Find and iterate through all the wines listed on the page
+  const wineElements = document.getElementsByClassName('mb-0 text-sanchez font-weight-bold');
+  for (var i = 0, l = wineElements.length; i < l; i++) {
+    insertRating(wineElements[i]);
+  }
 }
 
 // Retrieve the Vivino rating and update the wine name element
 function insertRating(wineNameElement) {
-  var wineName = wineNameElement.outerText;
-  console.log(wineName);
-  
+  const wineName = wineNameElement.outerText;
+
   return new Promise((resolve, reject) => {
     chrome.runtime.sendMessage(
       {contentScriptQuery: "queryWine", wineName: wineName}, response => {
         if(response) {
-          wineNameElement.innerHTML = wineName + '\n(Rating: ' + response + ")";
+          wineNameElement.innerHTML = wineName + '<br>(Rating: ' + response[0] + ", Reviews: " + response[1] + ")";
           resolve(response)
         } else {
-          reject('Something wrong');
+          reject('Encountered an error!');
         }
     });
   });
